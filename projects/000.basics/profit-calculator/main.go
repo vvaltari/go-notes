f2b47a0273a52c9790613afa6df769473d1d0ece
jonvikrr <studiesec.hula878@passmail.net>
2025-03-30 12:24:48 -0300

    feat: add function that calculates profit

--- projects/000.basics/profit-calculator/main.go
+++ projects/000.basics/profit-calculator/main.go
@@ -16,13 +16,19 @@ func main() {
 	fmt.Print("Tax Rate: ")
 	fmt.Scan(&taxRate)
 
-	ebt := revenue - expenses
-	profit := ebt * (1 - (taxRate / 100))
-	ratio := ebt / profit
+	ebt, profit, ratio := calcProfit(revenue, expenses, taxRate)
 
 	fmt.Println()
 
 	fmt.Printf("EBT: %.2f\n", ebt)
 	fmt.Printf("Profit: %.2f\n", profit)
 	fmt.Printf("Ratio: %.2f\n", ratio)
+}
+
+func calcProfit(revenue, expenses, taxRate float64) (ebt, profit, ratio float64) {
+	ebt = revenue - expenses
+	profit = ebt * (1 - (taxRate / 100))
+	ratio = ebt / profit	
+
+	return ebt, profit, ratio
 }
